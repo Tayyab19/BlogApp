6b9730fe3d59d439bc50b54f8f5c72db89aed285
anserbt <60539288+anserbt@users.noreply.github.com>
2022-05-16 16:36:44 +0500

    Prettier Formatting

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,28 +1,24 @@
-var express = require('express');
-const { default: mongoose } = require('mongoose');
+var express = require("express");
+const { default: mongoose } = require("mongoose");
 var router = express.Router();
-const blog = require('../models/blog');
+const blog = require("../models/blog");
 
 // Return all blogs in database
-router.get('/allBlogs/:username', async function(req, res, next) {
+router.get("/allBlogs/:username", async function (req, res, next) {
   const allBlogs = await blog.find({});
-  const BlogsWihtoutCurrUser = allBlogs.filter(b => {
-    return (b.username != req.params.username);
-  });
-  res.json(BlogsWihtoutCurrUser);
+  res.json(allBlogs);
 });
 
 // Return all blogs of particular user in database
-router.get('/blogs/:username', async function(req, res, next) {
-  const BlogsByID = await blog.find({username: req.params.username});
+router.get("/blogs/:username", async function (req, res, next) {
+  const BlogsByID = await blog.find({ username: req.params.username });
   res.json(BlogsByID);
 });
 
 // Delete blog from database
-router.delete('/deleteBlog/:id', async function(req, res, next) {
-  await blog.deleteOne({blog_id: req.params.id});
+router.delete("/deleteBlog/:id", async function (req, res, next) {
+  await blog.deleteOne({ blog_id: req.params.id });
   res.end("Deleted");
 });
-
 
 module.exports = router;
