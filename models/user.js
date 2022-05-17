dd8aa590605e13b36342ecfef025aef707a350ef
ASUS-ROG-STRIX\TAYYA <tayyab19jr@gmail.com>
2022-05-18 02:11:01 +0500

    no message

---
+++ models/user.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema({
+    username: {type: String, minlength: 5, required: true},
+    password: {type: String, select: false, minlength: 6, required: true},
+    email: {type: String, required: true},
+});
+
+module.exports = mongoose.model('users', schema);
