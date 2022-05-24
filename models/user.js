86016a1c4eefa12f4a3b1472f9fc7bec9d50ed36
anserbt <60539288+anserbt@users.noreply.github.com>
2022-05-24 13:09:29 +0500

    Blog App Complete

--- models/user.js
+++ models/user.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const schema = mongoose.Schema({
-    username: {type: String, minlength: 5, required: true},
-    password: {type: String, select: false, minlength: 6, required: true},
-    email: {type: String, required: true},
+  username: { type: String, minlength: 5, required: true },
+  password: { type: String, select: false, minlength: 6, required: true },
+  email: { type: String, minlength: 5, required: true },
 });
 
-module.exports = mongoose.model('users', schema);
+module.exports = mongoose.model("users", schema);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,45 @@
-var express = require('express');
+var express = require("express");
+const user = require("../models/user");
 var router = express.Router();
+const users = require("../models/user");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/:id/:password", async function (req, res, next) {
+  const currentUser = await users.findOne({
+    email: req.params.id,
+    password: req.params.password,
+  });
+
+  if (currentUser != null) {
+    res.send(currentUser.username);
+  } else {
+    res.send("false");
+  }
 });
+
+router.put(
+  "/register/:id/:password/:username",
+  async function (req, res, next) {
+    const existingemail = await users.findOne({
+      email: req.params.id,
+    });
+    const existingUsername = await users.findOne({
+      username: req.params.username,
+    });
+
+    if (existingemail != null || existingUsername != null) {
+      res.send("false");
+    } else {
+      let currentUser = {
+        email: req.params.id,
+        password: req.params.password,
+        username: req.params.username,
+      };
+      await users.create(currentUser);
+
+      res.send("true");
+    }
+  }
+);
 
 module.exports = router;
